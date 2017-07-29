d135ccaacbf5384967f22eb3edbde8602b9f37b4
Mohammad Yaseen <mohammadyaseen223@gmail.com>
2017-07-29 17:00:46 +0500

    Countdown Timer is ready with simple html css and JavaScript

---
+++ app.js
@@ -0,0 +1,30 @@
+function countdownCounter() {
+    var now = new Date();
+    var eventDate = new Date(2017, 7, 13);
+
+    var currentTime = now.getTime();
+    var eventTime = eventDate.getTime();
+
+    var remTime = eventTime - currentTime;
+
+    var s = Math.floor(remTime / 1000);
+    var m = Math.floor(s / 60);
+    var h = Math.floor(m / 60);
+    var d = Math.floor(h / 24);
+
+    h %= 24;
+    m %= 60;
+    s %= 60;
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    document.getElementById("days").innerText = d;
+    document.getElementById("hours").innerText = h;
+    document.getElementById("minutes").innerText = m;
+    document.getElementById("seconds").innerText = s;
+    
+    setTimeout(countdownCounter, 1000);
+}
+countdownCounter()
